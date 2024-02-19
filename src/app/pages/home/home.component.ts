2c45c5578c07e754bbf7538822a04e79d43977bc
jeissonR115 <jeissonfeliper@gmail.com>
2024-02-19 07:16:28 -0500

    add changeHandler(),deleteTask()

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -14,5 +14,15 @@ export class HomeComponent {
     'Almorzar',
     'Cenar',
     'Dormir',
-  ])
+    'asdf'
+  ]);
+  changeHandler(event:Event){
+    const input = event.target as HTMLInputElement;
+    const newTask = input.value;
+    this.tasks.update(task =>[...task,newTask]);
+    input.value = ''
+  }
+  deleteTask(index: number){
+    this.tasks.update(task => task.filter((task,position)=> position !== index));
+  }
 }
